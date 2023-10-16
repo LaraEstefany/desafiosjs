1609e4c9336450f134384e04f8c96787541c7093
Lara_Estefany <lara.estefanypc@gmail.com>
2023-10-16 10:24:25 -0300

    Refactor de código para facilitar leitura

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
-const listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const listaNumerosDois = [20, 50, 1, 100, 2, 6, 5, 3, 4, 200, -20, -1, -200];
-const frase = `O rato roeu a roupa do rei de Roma`;
+const numerosBaseUmADez = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numerosBaseAleatorio = [20, 50, 1, 100, 2, 6, 5, 3, 4, 200, -20, -1, -200];
+const fraseBase = `O rato roeu a roupa do rei de Roma`;
 
 /* 1. Print all even numbers from 0 – 10 */
 
-function numerosPares(num) {
+function retornaNumerosParesDeZeroALimite(limite) {
     const listaNumeros = [];
-    const listaNova = [];
+    const numerosPares = [];
 
-    for (let i = 0; i < num + 1; i++) {
+    for (let i = 0; i < limite + 1; i++) {
         listaNumeros.push(i);
     }
 
     for (const numero of listaNumeros) {
-        if (numero % 2 == 0) listaNova.push(numero);
+        if (numero % 2 == 0) numerosPares.push(numero);
     }
 
-    return listaNova;
+    return numerosPares;
 }
 
 /* 2. Print a table containing multiplication tables */
 
-function tabuada(num) {
-    const listaNova = [];
+function tabuada(multiplicador) {
+    const tabuadaCompleta = [];
 
-    for (const numero of listaNumeros) {
-        let frase = `${numero} x ${num} = ${numero * num}`;
-        listaNova.push(frase);
+    for (const numero of numerosBaseUmADez) {
+        let frase = `${numero} x ${multiplicador} = ${numero * multiplicador}`;
+        tabuadaCompleta.push(frase);
     }
 
-    return listaNova;
+    return tabuadaCompleta;
 }
 
 /* 
 3. Create a length converter function that convert from kilometers to miles. The function should include the input in kilometers and return the answer in miles.
 */
 
-function conversorKm(km) {
-    const conta = km / 1.609;
-    return `${km} km são ${conta.toFixed(4)} milhas.`;
+function conversorKmParaMilhas(km) {
+    const milhas = km / 1.609;
+    return `${km} km são ${milhas.toFixed(4)} milhas.`;
 }
 
 /*
@@ -48,7 +48,7 @@ You can create your own array of numbers but consider trying this problem with a
 Bonus intermediate challenge: Create a function that can return the sum of a particular column or row number in a table.
 */
 
-function soma(numeroInicial, numeroFinal) {
+function somaArrayDeNumeros(numeroInicial, numeroFinal) {
     const listaNumeros = [];
 
     for (let i = numeroInicial; i < numeroFinal + 1; i++) {
@@ -63,18 +63,18 @@ function soma(numeroInicial, numeroFinal) {
 
 /* 5. Create a function that reverses an array */
 
-const reverte = lista => lista.reverse();
+const reverteArray = lista => lista.reverse();
 
 /* 6. Sort an array from lowest to highest */
 
-function compara(lista) {
+function retornaArrayEmOrdemCrescente(lista) {
     lista.sort((a, b) => a - b);
     return lista;
 }
 
 /* 7. Create a function that filters out negative numbers */
 
-function retiraNegativos(lista) {
+function filtraNumerosNegativosDeArray(lista) {
     const listaNova = []
     for (const numero of lista) {
         if (numero >= 0) listaNova.push(numero);
@@ -84,48 +84,48 @@ function retiraNegativos(lista) {
 
 /* 8. Remove the spaces found in a string */
 
-const removeEspaço = frase => frase.split(' ').join('');
+const removeEspacosDeStrings = frase => frase.split(' ').join('');
 
 /* 9. Return a Boolean if a number is divisible by 10 */
 
-const divisivelPorDez = num => num % 10 == 0;
+const retornaSeNumeroForDivisivelPorDez = num => num % 10 == 0;
 
 /*
 10. Return the number of vowels in a string
 Create a function that'll return an integer of the number of vowels found in a string. This is a great way to practice determining the features of a dataset. If you use JavaScript later in your career, you'll be well-prepared to determine what datasets (or just strings) consist of. If you feel like an extra challenge, consider returning the number of characters.
 */
 
-function contaLetras(frase, letra) {
+function contaLetrasDeString(frase, letra) {
     frase = frase.toLowerCase();
     letra = letra.toLowerCase();
+
     let contador = 0;
     let inicio = frase.indexOf(`${letra}`);
 
     while (inicio != -1) {
         contador++;
         inicio = frase.indexOf(`${letra}`, inicio + 1);
-
     }
 
     return contador;
 }
 
-function contaVogais(frase) {
+function contaQuantasVogaisTemAString(frase) {
     const vogais = ['a', 'e', 'i', 'o', 'u'];
     let total = 0;
     for (const vogal of vogais) {
-        total = total + contaLetras(frase, vogal);
+        total = total + contaLetrasDeString(frase, vogal);
     }
     return total;
 }
 
-console.log(numerosPares(10));
+console.log(retornaNumerosParesDeZeroALimite(10));
 console.log(tabuada(7));
-console.log(conversorKm(1));
-console.log(soma(10, 200));
-console.log(reverte(listaNumeros));
-console.log(compara(listaNumerosDois));
-console.log(retiraNegativos(listaNumerosDois));
-console.log(removeEspaço(frase));
-console.log(divisivelPorDez(5));
-console.log(contaVogais(frase));
+console.log(conversorKmParaMilhas(1));
+console.log(somaArrayDeNumeros(10, 200));
+console.log(reverteArray(numerosBaseUmADez));
+console.log(retornaArrayEmOrdemCrescente(numerosBaseAleatorio));
+console.log(filtraNumerosNegativosDeArray(numerosBaseAleatorio));
+console.log(removeEspacosDeStrings(fraseBase));
+console.log(retornaSeNumeroForDivisivelPorDez(5));
+console.log(contaQuantasVogaisTemAString(fraseBase));
